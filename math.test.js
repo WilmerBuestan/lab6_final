68ae5013a2e59640061edf52b6713f3811a49980
WilmerBuestan <wobuestan@espe.edu.ec>
2026-02-07 15:29:15 -0500

    Fix: Corregido error en test factorial

--- math.test.js
+++ math.test.js
@@ -1,7 +1,7 @@
 const { factorial, fibonacci } = require('./math');
 
 test('Factorial de 5 es 120', () => {
-    expect(factorial(5)).toBe(9999999990);
+    expect(factorial(5)).toBe(120);
 });
 
 test('Fibonacci de 6 es 8', () => {
